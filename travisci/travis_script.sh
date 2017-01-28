d87950677e3a1e65eaac8f803d0e3def5d965e2f
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-01-28 21:15:08 +0100

    improve npm script names #15

--- travisci/travis_script.sh
+++ travisci/travis_script.sh
@@ -2,10 +2,16 @@
 
 # run debug build
 echo "npm run debug build on $TRAVIS_OS_NAME"
-npm run buildDev
+npm run build:dev
+
 # run production build
 echo "npm run production build on $TRAVIS_OS_NAME"
-npm run build
+npm run build:prod
+
+# run production + AOT build
+echo "npm run production + AOT build on $TRAVIS_OS_NAME"
+npm run build:prod:aot
+
 # run test
 echo "npm run test on $TRAVIS_OS_NAME"
 npm test
