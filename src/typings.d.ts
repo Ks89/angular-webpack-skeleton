22cbb542049dc5180def0fb9a73d907a5d7728eb
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-02-26 21:09:52 +0100

    refatoring and cleanup

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -22,16 +22,23 @@
  * SOFTWARE.
  */
 
+// support NodeJS modules without type definitions
+declare module '*';
+
+// Extra variables that live on Global that will be replaced by webpack DefinePlugin
+declare var ENV: string;
+declare var System: SystemJS;
+
+interface SystemJS {
+  import: (path?: string) => Promise<any>;
+}
+
 // JQuery
 interface JQuery {
   carousel(fn: string): any;
 }
 
-// Webpack
-// declare function require(path: string);
+// webpack
 declare const webpack: {
   ENV: string
 };
-
-// declare var require: any;
-declare var System: any;
