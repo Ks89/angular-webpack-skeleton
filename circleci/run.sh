c2850e32931b2ce3010d704c3fb771ae3b207609
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-04-01 16:58:52 +0200

    circle ci experiments

---
+++ circleci/run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# run debug build
+echo "npm run debug build on $TRAVIS_OS_NAME"
+npm run build:dev
+
+# run production build
+echo "npm run production build on $TRAVIS_OS_NAME"
+npm run build:prod
+
+# clean before the real production build
+npm run clean
+
+# run production + AOT build
+echo "npm run production + AOT build on $TRAVIS_OS_NAME"
+npm run build:prod:aot
+
+# run test
+# TODO add test here
+#echo "npm run test on $TRAVIS_OS_NAME"
+#npm test
+
+# TODO add protractor tests with npm run webdriver:update && npm run e2e
