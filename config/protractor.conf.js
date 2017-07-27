01e18114af93a46f946e5a074934c09086b5d160
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-07-28 01:03:28 +0200

    support e2e testing with Headless Chrome #40

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -59,7 +59,7 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
     'chromeOptions': {
-      'args': ['show-fps-counter=true']
+      args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
     }
   },
 
