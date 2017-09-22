45f82cd439b6d86979b2538be8409df971e8c030
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-09-22 22:24:45 +0200

    add manifest.json #48

--- config/head-config.common.js
+++ config/head-config.common.js
@@ -35,9 +35,11 @@ module.exports = {
     // { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/assets/icon/favicon-32x32.png' },
     // { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/assets/icon/favicon-96x96.png' },
     // { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/assets/icon/favicon-16x16.png' },
-    //
-    // /** <link> tags for a Web App Manifest **/
-    // { rel: 'manifest', href: '/assets/manifest.json' },
+
+    /**
+     * <link> tags for a Web App Manifest
+     */
+    { rel: 'manifest', href: '/assets/manifest.json' },
 
     /** font-awesome css **/
     { rel: 'stylesheet', href: '/assets/font-awesome/css/font-awesome.min.css' }
